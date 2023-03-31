e9900a6f1321bd281ebc722ffc3a0b2f196a7ca3
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-03-31 16:47:26 -0400

    Move tests.

--- testroom.py
+++ testroom.py
@@ -1,5 +1,5 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#import os, sys
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
 from game import Room
 
