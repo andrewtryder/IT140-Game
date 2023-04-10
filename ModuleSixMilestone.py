cd3d8fa207c0a028b7a856d46808937607cfc879
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-04-09 20:17:29 -0400

    pylint on ModuleSixMilestone.py

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -1,34 +1,38 @@
-# Andrew Ryder
-# IT140 - ModuleSixMilestone.py
+"""ModuleSixMilestone.py
+Andrew Ryder
+"""
 
-#A dictionary for the simplified dragon text game
-#The dictionary links a room to other rooms.
+# disable warning about pylint not liking the file name.
+# pylint: disable-msg=C0103
+
+# A dictionary for the simplified dragon text game
+# The dictionary links a room to other rooms.
 rooms = {
         'Great Hall': {'South': 'Bedroom'},
         'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
         'Cellar': {'West': 'Bedroom'}
 }
 
-#initialize current room to Great Hall
-current_room = 'Great Hall'
+# initialize current room to Great Hall
+CURRENT_ROOM = 'Great Hall'
 
-#Gameplay loop
-while current_room != 'exit':
-    #display current room
-    print('You are currently in the', current_room)
-    
-    #ask for user input
+# Gameplay loop
+while CURRENT_ROOM != 'exit':
+    # display current room
+    print('You are currently in the', CURRENT_ROOM)
+
+    # ask for user input
     direction = input('Which direction do you want to go? (North, South, East, West, or exit) ')
-    
-    #handle user input
+
+    # handle user input
     if direction == 'exit':
-        #end game if player enters "exit"
-        current_room = 'exit'
+        # end game if player enters "exit"
+        CURRENT_ROOM = 'exit'
         print('Thanks for playing!')
-    elif direction in rooms[current_room]:
-        #move player to new room if direction is valid
-        current_room = rooms[current_room][direction]
-        print('You have moved', direction, 'to the', current_room)
+    elif direction in rooms[CURRENT_ROOM]:
+        # move player to new room if direction is valid
+        CURRENT_ROOM = rooms[CURRENT_ROOM][direction]
+        print('You have moved', direction, 'to the', CURRENT_ROOM)
     else:
-        #invalid input, ask again
+        # invalid input, ask again
         print('Invalid direction, please try again.')
