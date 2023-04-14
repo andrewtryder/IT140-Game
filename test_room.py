8138654338146eca21e9ca44a634a96be065083e
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-04-14 08:12:24 -0400

    single blank line between funcs in test_room.py

--- test_room.py
+++ test_room.py
@@ -15,7 +15,6 @@ class TestRoom(unittest.TestCase):
         """
         self.room = Room("Test Room", "This is a test room.")
 
-
     def test_init(self):
         """
         Tests that the room is initialized correctly.
@@ -24,7 +23,6 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(self.room.description, "This is a test room.")
         self.assertEqual(self.room.items, [])
         self.assertEqual(self.room.exits, {})
-
 
     def test_add_exit(self):
         """
@@ -35,7 +33,6 @@ class TestRoom(unittest.TestCase):
         self.assertIsNotNone(self.room.get_exit("north"))
         self.assertIsNone(self.room.get_exit("south"))
 
-
     def test_add_item(self):
         """
         Tests that items are added correctly.
@@ -43,7 +40,6 @@ class TestRoom(unittest.TestCase):
         self.room.add_item("test item")
         self.assertEqual(len(self.room.items), 1)
         self.assertIn("test item", self.room.items)
-
 
     def test_remove_item(self):
         """
@@ -53,7 +49,6 @@ class TestRoom(unittest.TestCase):
         self.room.remove_item("test item")
         self.assertEqual(len(self.room.items), 0)
         self.assertNotIn("test item", self.room.items)
-
 
     def test_move(self):
         """
